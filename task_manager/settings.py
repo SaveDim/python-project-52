9784edb9d5ccad5b139db6fd406563f8fc730f95
savedim <savedimdev@gmail.com>
2023-12-05 11:47:36 +0300

    Change dedbug mode

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from distutils.util import strtobool
 from pathlib import Path
 
-import dj_database_url
 from django.utils.translation import gettext_lazy as _
+
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -31,9 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = bool(strtobool(os.getenv('DEBUG', False)))
-# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 LOCAL_HOST = os.getenv('HOST')
