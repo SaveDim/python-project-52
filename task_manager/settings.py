00fd69cdbc9cfe041d8241697670f5063eb0631c
savedim <savedimdev@gmail.com>
2023-12-05 10:29:15 +0300

    Add host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,6 +38,7 @@ LOCAL_HOST = os.getenv('HOST')
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  '0.0.0.0'
+                 'webserver'
                  'LOCAL_HOST',
                  ]
 
