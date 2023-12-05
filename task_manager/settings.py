c94b58bf59339d5ca63347f156d0c47ab3ed860a
savedim <savedimdev@gmail.com>
2023-12-05 12:52:22 +0300

    Change dedbug mode

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-# DEBUG = True
+DEBUG = True
 
 
 LOCAL_HOST = os.getenv('HOST')
