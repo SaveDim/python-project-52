08b0c0ff50b40b38a9cb53b2c8dcdbc8e5752ecb
savedim <savedimdev@gmail.com>
2023-12-05 11:04:13 +0300

    Add host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,18 +33,19 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-LOCAL_HOST = os.getenv('HOST')
+# LOCAL_HOST = os.getenv('HOST')
 
 ALLOWED_HOSTS = [
      'localhost',
-     # '127.0.0.1',
+     '127.0.0.1',
      'webserver'
      'LOCAL_HOST',
+     'RENDER_EXTERNAL_HOSTNAME'
 ]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
