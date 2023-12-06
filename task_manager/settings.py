a688ec75c2e29cec9b5ce4059bd91d9e2a4f7ca4
savedim <savedimdev@gmail.com>
2023-12-06 14:21:53 +0300

    Change staticfiles storage

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -170,10 +170,6 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # if not DEBUG:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
