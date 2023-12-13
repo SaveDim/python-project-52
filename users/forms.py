81a05233c58922a6aae09df0a3373823f26a7db7
savedim <savedimdev@gmail.com>
2023-12-13 13:48:23 +0300

    Add minor fixes

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import forms
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+from django.core.validators import MinLengthValidator
 
 
 class UserCreateForm(UserCreationForm):
@@ -8,3 +11,64 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
+        password_min_len = 3
+        #
+        # first_name = forms.CharField(
+        #     label=_('Name'),
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             'class': 'form-control',
+        #             'placeholder': _('Name'),
+        #         }
+        #     )
+        # )
+        #
+        # last_name = forms.CharField(
+        #     label=_('Surname'),
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             'class': 'form-control',
+        #             'placeholder': _('Surname'),
+        #         }
+        #     )
+        # )
+        #
+        # username = forms.CharField(
+        #     label=_('Username'),
+        #     widget=forms.TextInput(
+        #         attrs={
+        #             'class': 'form-control',
+        #             'placeholder': _('Username'),
+        #             'title': _('Not an option')
+        #         }
+        #     )
+        # )
+        #
+        # password1 = forms.CharField(
+        #     validators=[
+        #         MinLengthValidator(password_min_len,
+        #                            _("Password is too short")),
+        #     ],
+        #     label=_('Password'),
+        #     widget=forms.PasswordInput(
+        #         attrs={
+        #             'class': 'form-control',
+        #             'placeholder': _('Password'),
+        #             'title': _('Password must contains at least 3 chars')
+        #         }
+        #     )
+        # )
+        # password2 = forms.CharField(
+        #     label=_('Password confirmation'),
+        #     validators=[
+        #         MinLengthValidator(password_min_len,
+        #                            _("Password is too short")),
+        #     ],
+        #     widget=forms.PasswordInput(
+        #         attrs={
+        #             'class': 'form-control',
+        #             'placeholder': _('Password confirmation'),
+        #             'title': _('Please, type your password again'),
+        #         }
+        #     )
+        # )
